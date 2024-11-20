922f1ec085ecf6cbc999c1ec2943fa61798453de
Raphael Pusch <raphael.pusch@ai-research-office.com>
2024-11-20 16:10:30 +0000

    code fixed to actual eeprom size data sets one more new data set recorded

--- src/main.cpp
+++ src/main.cpp
@@ -11,8 +11,8 @@ void setup() {
   Serial.begin(9600);
   Wire.begin();
   byte data;
-  for(int j = 0; j < 16; j++){
-    for(int i = 0; i < 16; i++){
+  for(int j = 0; j < 32; j++){
+    for(int i = 0; i < 8; i++){
       data = readI2CByte(j*8 + i);
       if (data <= 15){
         Serial.print("0");
@@ -38,10 +38,414 @@ void setup() {
   if(digitalRead(2) == HIGH){
     Serial.print("Writing Data\n");
     uint8_t* data_to_send = new uint8_t[8];
-    for (int i = 0; i < 512; i++){
+    for (int i = 0; i < 8; i++){
       data_to_send[i] = 0xFF;
     } 
-    data_to_send[0] = 0x40;
+    data_to_send[0] = 0x01;
+    data_to_send[1] = 0x02;
+    data_to_send[2] = 0x03;
+    data_to_send[3] = 0x04;
+    data_to_send[4] = 0x4B;
+    data_to_send[5] = 0xAC;
+    data_to_send[6] = 0x81;
+    data_to_send[7] = 0x38;
+
+    Wire.beginTransmission(ADDR);
+    Wire.write(0x00);
+    Wire.write(data_to_send, 8);
+    Wire.endTransmission();
+
+    delay(50);
+
+    data_to_send[0] = 0xFA;
+    data_to_send[1] = 0xD3;
+    data_to_send[2] = 0x59;
+    data_to_send[3] = 0x7D;
+    data_to_send[4] = 0x62;
+    data_to_send[5] = 0xB4;
+    data_to_send[6] = 0x11;
+    data_to_send[7] = 0xA5;
+
+    Wire.beginTransmission(ADDR);
+    Wire.write(0x08);
+    Wire.write(data_to_send, 8);
+    Wire.endTransmission();
+
+    delay(50);
+
+    data_to_send[0] = 0x0A;
+    data_to_send[1] = 0x1F;
+    data_to_send[2] = 0xFF;
+    data_to_send[3] = 0xFF;
+    data_to_send[4] = 0xFF;
+    data_to_send[5] = 0xFF;
+    data_to_send[6] = 0xFF;
+    data_to_send[7] = 0xFF;
+    
+    Wire.beginTransmission(ADDR);
+    Wire.write(0x10);
+    Wire.write(data_to_send, 8);
+    Wire.endTransmission();
+
+    delay(50);
+
+    data_to_send[0] = 0xFF;
+    data_to_send[1] = 0xFF;
+    data_to_send[2] = 0xFF;
+    data_to_send[3] = 0xFF;
+    data_to_send[4] = 0xFF;
+    data_to_send[5] = 0xFF;
+    data_to_send[6] = 0xFF;
+    data_to_send[7] = 0xFF;
+
+    Wire.beginTransmission(ADDR);
+    Wire.write(0x18);
+    Wire.write(data_to_send, 8);
+    Wire.endTransmission();
+
+    delay(50);
+
+    data_to_send[0] = 0x0B;
+    data_to_send[1] = 0xCE;
+    data_to_send[2] = 0xE7;
+    data_to_send[3] = 0x06;
+    data_to_send[4] = 0xFF;
+    data_to_send[5] = 0xFF;
+    data_to_send[6] = 0xFF;
+    data_to_send[7] = 0xFF;
+
+    Wire.beginTransmission(ADDR);
+    Wire.write(0x20);
+    Wire.write(data_to_send, 8);
+    Wire.endTransmission();
+
+    delay(50);
+
+    data_to_send[0] = 0xFF;
+    data_to_send[1] = 0xFF;
+    data_to_send[2] = 0xFF;
+    data_to_send[3] = 0xFF;
+    data_to_send[4] = 0xFF;
+    data_to_send[5] = 0xFF;
+    data_to_send[6] = 0xFF;
+    data_to_send[7] = 0xFF;
+
+    Wire.beginTransmission(ADDR);
+    Wire.write(0x28);
+    Wire.write(data_to_send, 8);
+    Wire.endTransmission();
+
+    delay(50);
+
+    data_to_send[0] = 0xFF;
+    data_to_send[1] = 0xFF;
+    data_to_send[2] = 0xFF;
+    data_to_send[3] = 0xFF;
+    data_to_send[4] = 0xFF;
+    data_to_send[5] = 0xFF;
+    data_to_send[6] = 0xFF;
+    data_to_send[7] = 0xFF;
+
+    Wire.beginTransmission(ADDR);
+    Wire.write(0x30);
+    Wire.write(data_to_send, 8);
+    Wire.endTransmission();
+
+    delay(50);
+
+    data_to_send[0] = 0xFF;
+    data_to_send[1] = 0xFF;
+    data_to_send[2] = 0xFF;
+    data_to_send[3] = 0xFF;
+    data_to_send[4] = 0xFF;
+    data_to_send[5] = 0xFF;
+    data_to_send[6] = 0xFF;
+    data_to_send[7] = 0xFF;
+
+    Wire.beginTransmission(ADDR);
+    Wire.write(0x38);
+    Wire.write(data_to_send, 8);
+    Wire.endTransmission();
+
+    delay(50);
+
+    data_to_send[0] = 0xFF;
+    data_to_send[1] = 0xFF;
+    data_to_send[2] = 0xFF;
+    data_to_send[3] = 0xFF;
+    data_to_send[4] = 0xF8;
+    data_to_send[5] = 0xCD;
+    data_to_send[6] = 0xE7;
+    data_to_send[7] = 0x06;
+
+    Wire.beginTransmission(ADDR);
+    Wire.write(0x40);
+    Wire.write(data_to_send, 8);
+    Wire.endTransmission();
+
+    delay(50);
+
+    data_to_send[0] = 0x65;
+    data_to_send[1] = 0x00;
+    data_to_send[2] = 0x27;
+    data_to_send[3] = 0x01;
+    data_to_send[4] = 0xE9;
+    data_to_send[5] = 0x00;
+    data_to_send[6] = 0x09;
+    data_to_send[7] = 0x01;
+
+    Wire.beginTransmission(ADDR);
+    Wire.write(0x48);
+    Wire.write(data_to_send, 8);
+    Wire.endTransmission();
+
+    delay(50);
+
+    data_to_send[0] = 0x2B;
+    data_to_send[1] = 0x00;
+    data_to_send[2] = 0x00;
+    data_to_send[3] = 0x00;
+    data_to_send[4] = 0xFF;
+    data_to_send[5] = 0xFF;
+    data_to_send[6] = 0xFF;
+    data_to_send[7] = 0xFF;
+
+    Wire.beginTransmission(ADDR);
+    Wire.write(0x50);
+    Wire.write(data_to_send, 8);
+    Wire.endTransmission();
+
+    delay(50);
+
+    data_to_send[0] = 0xFF;
+    data_to_send[1] = 0xFF;
+    data_to_send[2] = 0xFF;
+    data_to_send[3] = 0xFF;
+    data_to_send[4] = 0xFF;
+    data_to_send[5] = 0xFF;
+    data_to_send[6] = 0xFF;
+    data_to_send[7] = 0xFF;
+
+    Wire.beginTransmission(ADDR);
+    Wire.write(0x58);
+    Wire.write(data_to_send, 8);
+    Wire.endTransmission();
+
+    delay(50);
+
+    Wire.beginTransmission(ADDR);
+    Wire.write(0x60);
+    Wire.write(data_to_send, 8);
+    Wire.endTransmission();
+
+    delay(50);
+
+    Wire.beginTransmission(ADDR);
+    Wire.write(0x68);
+    Wire.write(data_to_send, 8);
+    Wire.endTransmission();
+
+    delay(50);
+
+    Wire.beginTransmission(ADDR);
+    Wire.write(0x70);
+    Wire.write(data_to_send, 8);
+    Wire.endTransmission();
+
+    delay(50);
+
+    Wire.beginTransmission(ADDR);
+    Wire.write(0x78);
+    Wire.write(data_to_send, 8);
+    Wire.endTransmission();
+
+    delay(50);
+
+    Wire.beginTransmission(ADDR);
+    Wire.write(0x80);
+    Wire.write(data_to_send, 8);
+    Wire.endTransmission();
+
+    delay(50);
+
+    Wire.beginTransmission(ADDR);
+    Wire.write(0x88);
+    Wire.write(data_to_send, 8);
+    Wire.endTransmission();
+
+    delay(50);
+
+    Wire.beginTransmission(ADDR);
+    Wire.write(0x90);
+    Wire.write(data_to_send, 8);
+    Wire.endTransmission();
+
+    delay(50);
+
+    Wire.beginTransmission(ADDR);
+    Wire.write(0x98);
+    Wire.write(data_to_send, 8);
+    Wire.endTransmission();
+
+    delay(50);
+
+    Wire.beginTransmission(ADDR);
+    Wire.write(0xA0);
+    Wire.write(data_to_send, 8);
+    Wire.endTransmission();
+
+    delay(50);
+
+    Wire.beginTransmission(ADDR);
+    Wire.write(0xA8);
+    Wire.write(data_to_send, 8);
+    Wire.endTransmission();
+
+    delay(50);
+
+    data_to_send[0] = 0xFF;
+    data_to_send[1] = 0xFF;
+    data_to_send[2] = 0xFF;
+    data_to_send[3] = 0xFF;
+    data_to_send[4] = 0xFF;
+    data_to_send[5] = 0xFF;
+    data_to_send[6] = 0xFF;
+    data_to_send[7] = 0x01;
+
+    Wire.beginTransmission(ADDR);
+    Wire.write(0xB0);
+    Wire.write(data_to_send, 8);
+    Wire.endTransmission();
+
+    delay(50);
+
+    data_to_send[0] = 0xFF;
+    data_to_send[1] = 0xFF;
+    data_to_send[2] = 0xFF;
+    data_to_send[3] = 0xFF;
+    data_to_send[4] = 0xFF;
+    data_to_send[5] = 0xFF;
+    data_to_send[6] = 0xFF;
+    data_to_send[7] = 0xFF;
+
+    Wire.beginTransmission(ADDR);
+    Wire.write(0xB8);
+    Wire.write(data_to_send, 8);
+    Wire.endTransmission();
+
+    delay(50);
+
+    Wire.beginTransmission(ADDR);
+    Wire.write(0xC0);
+    Wire.write(data_to_send, 8);
+    Wire.endTransmission();
+
+    delay(50);
+
+    Wire.beginTransmission(ADDR);
+    Wire.write(0xC8);
+    Wire.write(data_to_send, 8);
+    Wire.endTransmission();
+
+    delay(50);
+
+    Wire.beginTransmission(ADDR);
+    Wire.write(0xD0);
+    Wire.write(data_to_send, 8);
+    Wire.endTransmission();
+
+    delay(50);
+
+    Wire.beginTransmission(ADDR);
+    Wire.write(0xD8);
+    Wire.write(data_to_send, 8);
+    Wire.endTransmission();
+
+    delay(50);
+
+    Wire.beginTransmission(ADDR);
+    Wire.write(0xE0);
+    Wire.write(data_to_send, 8);
+    Wire.endTransmission();
+
+    delay(50);
+
+    Wire.beginTransmission(ADDR);
+    Wire.write(0xE8);
+    Wire.write(data_to_send, 8);
+    Wire.endTransmission();
+
+    delay(50);
+
+    Wire.beginTransmission(ADDR);
+    Wire.write(0xF0);
+    Wire.write(data_to_send, 8);
+    Wire.endTransmission();
+
+    delay(50);
+
+    Wire.beginTransmission(ADDR);
+    Wire.write(0xF8);
+    Wire.write(data_to_send, 8);
+    uint8_t result = Wire.endTransmission();
+
+    Serial.println(result);
+
+    delete(data_to_send);
+    delay(100);
+
+    Wire.begin();
+    for(int j = 0; j < 32; j++){
+      for(int i = 0; i < 8; i++){
+        data = readI2CByte(j*8 + i);
+        if (data <= 15){
+          Serial.print("0");
+        }
+        Serial.print(data, HEX);
+        i++;
+        data = readI2CByte(j*8 + i);
+        if (data <= 15){
+          Serial.print("0");
+        }
+        Serial.print(data, HEX);
+        if(i == 15){
+          break;
+        }
+        Serial.print(" ");
+      }
+      Serial.print("\n");
+    }
+
+    Wire.endTransmission();
+  }
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
+
+void writeI2CByte(uint8_t data_addr, uint8_t data){
+  Wire.beginTransmission(ADDR);
+  Wire.write(data_addr);
+  Wire.write(data);
+  Wire.endTransmission();
+}
+
+byte readI2CByte(byte data_addr){
+  byte data = NULL;
+  Wire.beginTransmission(ADDR);
+  Wire.write(data_addr);
+  Wire.endTransmission();
+  Wire.requestFrom(ADDR, 1); //retrieve 1 returned byte
+  delay(1);
+  if(Wire.available()){
+    data = Wire.read();
+  }
+  return data;
+}
+
+/* Code Töpfchen 1
+data_to_send[0] = 0x40;
     data_to_send[1] = 0x55;
     data_to_send[2] = 0x51;
     data_to_send[3] = 0x01;
@@ -294,58 +698,4 @@ void setup() {
     Wire.write(0x78);
     Wire.write(data_to_send, 8);
     uint8_t result = Wire.endTransmission();
-    
-    Serial.println(result);
-
-    delete(data_to_send);
-    delay(100);
-
-    Wire.begin();
-    for(int j = 0; j < 16; j++){
-      for(int i = 0; i < 16; i++){
-        data = readI2CByte(j*8 + i);
-        if (data <= 15){
-          Serial.print("0");
-        }
-        Serial.print(data, HEX);
-        i++;
-        data = readI2CByte(j*8 + i);
-        if (data <= 15){
-          Serial.print("0");
-        }
-        Serial.print(data, HEX);
-        if(i == 15){
-          break;
-        }
-        Serial.print(" ");
-      }
-      Serial.print("\n");
-    }
-
-    Wire.endTransmission();
-  }
-}
-
-void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-void writeI2CByte(uint8_t data_addr, uint8_t data){
-  Wire.beginTransmission(ADDR);
-  Wire.write(data_addr);
-  Wire.write(data);
-  Wire.endTransmission();
-}
-
-byte readI2CByte(byte data_addr){
-  byte data = NULL;
-  Wire.beginTransmission(ADDR);
-  Wire.write(data_addr);
-  Wire.endTransmission();
-  Wire.requestFrom(ADDR, 1); //retrieve 1 returned byte
-  delay(1);
-  if(Wire.available()){
-    data = Wire.read();
-  }
-  return data;
-}
+*/
